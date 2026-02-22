d4b34923c8bdbc8ece7f69f16d107f53ef533f69
DanielHabenicht <daniel-habenicht@outlook.de>
2026-02-22 19:10:16 +0100

    use dev server

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,14 +3,17 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import replace from '@rollup/plugin-replace';
 import postcss from 'rollup-plugin-postcss';
+import serve from 'rollup-plugin-serve';
 
 process.env.SASS_SILENCE_DEPRECATIONS = 'legacy-js-api';
+const isDevServer = process.env.ROLLUP_DEV_SERVER === 'true';
+
 export default {
   input: 'src/familycalendar-card.ts',
   output: {
     file: 'dist/familycalendar-card.js',
     format: 'es',
-    sourcemap: 'inline',
+    sourcemap: true,
   },
   plugins: [
     replace({
@@ -34,5 +37,14 @@ export default {
       declaration: false,
       declarationMap: false,
     }),
+    isDevServer &&
+      serve({
+        contentBase: ['dist'],
+        host: '0.0.0.0',
+        port: 4000,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+      }),
   ],
 };
